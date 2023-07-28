2cffa09aff52a365cb803ef968d2ce629e4ca02a
yuki teramoto <yuki_teramoto@kidsline.co.jp>
2023-07-29 00:15:17 +0900

    bundle exec rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_07_23_142720) do
+ActiveRecord::Schema.define(version: 2023_07_28_150446) do
+
+  create_table "families", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "primary_user_id"
+  end
+
+  create_table "family_members", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "family_id"
+    t.string "email", null: false
+    t.boolean "family_admin_flag"
+    t.integer "relation"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "email", default: "", null: false
